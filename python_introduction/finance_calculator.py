5646d662d338f49df5526ab6b86676955f5ff11d
Ezekiel Kielson Sackey <ezekielkielsonsackey.github.com>
2025-07-13 00:09:57 -0700

    finance_calculator

---
+++ python_introduction/finance_calculator.py
@@ -0,0 +1,15 @@
+# Pronpt the user for their monthly income
+income = float(input('Enter your monthly income: '))
+
+# Ask for the total monthly exspenses
+expenses = float(input('Enter your total monthly expenses: '))
+
+# Calculate monthly savings
+monthly_savings = income - expenses
+
+# Project annual savings with 5% interest
+annual_savings = monthly_savings * 12 + (monthly_savings * 12 * 0.05)
+
+# Display the results
+print(f"Your monthly savings are ${monthly_savings:.2f}.")
+print(f"Projected savings after one year, with interest, is: ${annual_savings:.2f}.")
